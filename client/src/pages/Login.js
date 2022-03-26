4db8b12f77aa6bda75db6f228045f6a143de89ef
EdwardIzraitel <edward.izraital@hotmail.com>
2022-03-26 08:06:30 -0400

    auth working

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,26 +1,27 @@
 import React from "react";
 import styles from "./Login.module.scss";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Field, ErrorMessage } from "formik";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
-  const validateUser = (values) => {
-    axios
-      .post("http://127.0.0.1:8000/api/login", {
-        username: values.username,
-        password: values.password,
-      })
+  const validateUser = async (values) => {
+    const loginFormData = new FormData();
+    loginFormData.append("username", values.username);
+    loginFormData.append("password", values.password);
+
+    await axios
+      .post("http://127.0.0.1:8000/api/login", loginFormData)
       .then((res) => {
-        console.log(res);
-        alert(res);
-        navigate("/");
+        console.log(res); //.data
       })
       .catch((error) => {
+        console.log(error.message);
         alert(error);
       });
   };
+
   return (
     <div className={styles.center}>
       <div className={styles.titleContainer}>
@@ -31,14 +32,11 @@ const Login = () => {
         <div className={styles.loginContainer}>
           <Formik
             initialValues={{ username: "", password: "" }}
-            // validate={(values) => validateUser(values)}
-            onSubmit={(values, { setSubmitting }) => {
+            onSubmit={async (values) => {
               validateUser(values);
-              //   alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
             }}
           >
-            {({ values, handleSubmit, isSubmitting }) => (
+            {({ values, handleSubmit }) => (
               <form onSubmit={handleSubmit}>
                 <div className={styles.form_row}>
                   <label htmlFor="username" style={{ display: "block" }}>
@@ -60,9 +58,7 @@ const Login = () => {
                   />
                   <ErrorMessage name="password" component="div" />
                 </div>
-                <button type="submit" disabled={isSubmitting}>
-                  Submit
-                </button>
+                <button type="submit">Submit</button>
                 <p>Click to Register</p>
                 <div className={styles.dummyAccounts}>
                   <p>Dummy User</p>
