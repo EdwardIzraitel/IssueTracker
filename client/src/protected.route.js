25817bc45ea4a027d3ef02610a5d9dca695a05c9
EdwardIzraitel <edward.izraital@hotmail.com>
2022-04-05 19:13:10 -0400

    protected route continue

--- client/src/protected.route.js
+++ client/src/protected.route.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Navigate, Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import { Navigate, Outlet } from "react-router-dom";
 import { verifyToken } from "./app/api/userAPI";
 //TODO
 /*
@@ -9,23 +9,25 @@ import { verifyToken } from "./app/api/userAPI";
 3 -> complete protectedroute to validate routes
 
 */
-const ProtectedRoute = () => {
-  const verify = async () => {
-    return await verifyToken().then((res) => {
-      //   console.log(res);
-      return res;
-    });
-  };
-  //   const x = async () => {
-  //     const y = await verify();
-  //     // console.log(y);
-  //     return y;
-  //   };
-  const y = verify().then((value) => {
-    return value;
+const ProtectedRoute = ({ children }) => {
+  const [routeTo, setRouteTo] = useState(children);
+  // var routeTo = childr
+  //   var routeTo = children;
+  const c = verifyToken();
+
+  c.then((value) => {
+    console.log(value);
+    routeTo = value
+      ? setRouteTo((prev) => children)
+      : setRouteTo((prev) => <Navigate replace to="/login" />);
+  }).catch((err) => {
+    console.log(err);
+    setRouteTo((prev) => <Navigate replace to="/login" />);
   });
-  console.log(y);
-  return verify() ? <Outlet /> : <Navigate replace to="/login" />;
+
+  //   console.log(c);
+  return routeTo;
+  //   return verify() ? <Outlet /> : <Navigate replace to="/login" />;
 };
 
 export default ProtectedRoute;
